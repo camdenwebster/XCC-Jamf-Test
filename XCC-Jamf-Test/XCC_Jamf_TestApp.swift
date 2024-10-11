7c66fdaf89514fdb9a2fa65ac24cecf49977700a
Camden Webster <camden.webster@jamf.com>
2024-10-11 14:17:41 -0500

    Initial Commit

---
+++ XCC-Jamf-Test/XCC_Jamf_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCC_Jamf_TestApp.swift
+//  XCC-Jamf-Test
+//
+//  Created by Camden Webster on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct XCC_Jamf_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XCC-Jamf-TestTests/XCC_Jamf_TestTests.swift
@@ -0,0 +1,17 @@
+//
+//  XCC_Jamf_TestTests.swift
+//  XCC-Jamf-TestTests
+//
+//  Created by Camden Webster on 10/11/24.
+//
+
+import Testing
+@testable import XCC_Jamf_Test
+
+struct XCC_Jamf_TestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
